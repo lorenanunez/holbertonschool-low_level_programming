15d444b22f9940c8dcd32f05f5695464ea893726
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-15 14:33:51 -0300

    Exercise #4 fix

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -14,10 +14,10 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
- 	if (ch != 'q' && ch != 'e')
+		if (ch != 'q' && ch != 'e')
 		{
 			putchar(ch);
-		}	
+		}
 	}
 	putchar('\n');
 	return (0);
