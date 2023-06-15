ad8ccd4fda6191ba25a6c6a7d6647c48ad2e633a
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-15 14:15:33 -0300

    Exercise #2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Main function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	for (char ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
