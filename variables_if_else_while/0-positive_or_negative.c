6c59af6d2a9aa89c9c89b2243f2f3fa9541b7c2e
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-15 07:22:10 -0700

    Exercise #0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Main function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if (n > 0)
+	{
+		puts("is positive");
+	}
+	else if (n < 0)
+	{
+		puts("is negative");
+	}
+	else
+	{
+		puts("is zero");
+	}
+	
+	return (0);;
+}
