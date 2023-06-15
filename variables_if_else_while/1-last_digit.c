d2b05057ef06abaf685f44b5b5e73f8e3b28005c
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-15 13:56:34 -0300

    Exercise #1 fix

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 
 	if (lastDigit > 5)
 	{
-		printf("Las digit of %d is %d and is greater than 5\n");
+		printf("Las digit of %d is %d and is greater than 5\n", n, lastDigit);
 	}
 	else if (lastDigit == 0)
 	{
