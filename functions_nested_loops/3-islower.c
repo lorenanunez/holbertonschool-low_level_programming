f60ba1417ea9f02c63389c24cf5fc7b5e8d3a15a
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-16 11:02:02 -0300

    Exercise #3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Exercise 2 function
+ *
+ * Description: Exercise #2 function
+ *
+ * Return: Always null
+*/
+
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
