03e8cd4ef69800afd70e0e77ac6795f24e7f78ac
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-16 14:15:34 -0300

    Exercise #6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Exercise 6 function
+ * @c: Integer to determinate the absolute value
+ * Description: Determinates absolute value of c
+ *
+ * Return: Absolute value of c
+*/
+
+int _abs(int c)
+{
+	return (abs(c));
+}
