ed3829f2e7823ebd5b089aa3c7dae4528b155148
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-16 14:26:22 -0300

    Exercise #7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last:_digit - Exercise 7 function
+ * @num: Integer to print the last digit
+ * Description: Gives last digit of num
+ *
+ * Return: Always success (0)
+*/
+
+int print_last_digit(int num)
+{
+	int r;
+
+    r = num % 10;
+    _putchar(r);
+    return (0);
+}
