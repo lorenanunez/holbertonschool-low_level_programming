18afc6e42001e996e5a2c06d8de443a02b24925d
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-16 09:53:19 -0300

    Exercise #0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Main function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
