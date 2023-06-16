64bf26645ca9a5b6f3ecb70cc7fc146fc1b386b3
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-16 10:15:25 -0300

    Exercise #1 fix

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -8,7 +9,7 @@
  * Return: Always 0 (Success)
 */
 
-void print_alphabet()
+void print_alphabet(void)
 {
 	char ch;
 
