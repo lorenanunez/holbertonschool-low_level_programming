a7a719eefbc5674f7a63098fe0df7c055e37a518
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-20 14:16:43 -0300

    Exercise #5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - Exercise 4 function
+ *
+ * Description: Prints the numbers from 0 to 14
+ * 10 times
+ *
+ * Return: Always null
+*/
+
+void more_numbers(void)
+{
+	int i, x, firstDigit, secondDigit;
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int x = 0; x <= 14; x++)
+		{
+			firstDigit = x / 10;
+			secondDigit = x % 10;
+			if (firstDigit != 0)
+			{
+				putchar(48 + firstDigit);
+			}
+			putchar(48 + secondDigit);
+		}
+		putchar('\n');
+	}
+}
