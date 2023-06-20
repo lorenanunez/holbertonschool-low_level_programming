62874bb52dd8b633f1229a05ddf73a306216b415
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-20 15:21:25 -0300

    Exercise #9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * main - Exercise 9 function
+ *
+ * Description: Prints from 1 to 100. But for multiples
+ * of three print Fizz instead of the number and for the
+ * multiples of five print Buzz. For numbers which are
+ * multiples of both three and five print FizzBuzz.
+ *
+ * Return: Always null
+*/
+
+void main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	return (0);
+}
