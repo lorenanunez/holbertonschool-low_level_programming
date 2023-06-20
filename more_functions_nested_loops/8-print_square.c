e7d985124bbadce2c018806a971dbfc61b073a70
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-20 15:03:10 -0300

    Exercise #18

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Exercise 8 function
+ * @size: The size of the square
+ * Description: Prints a square, followed by a new line
+ *
+ * Return: Always null
+*/
+
+void print_square(int size)
+{
+	int i, x;
+
+	for (i = 0; i < size; i++)
+	{
+		for (x = 0; x < size; x++)
+		{
+			putchar('#');
+		}
+		putchar('\n');
+	}
+}
