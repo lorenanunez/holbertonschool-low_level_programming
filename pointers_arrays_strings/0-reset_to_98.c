7be099087bf57521adf981e77a64d25b8259f95c
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-21 11:00:49 -0300

    Exercise #0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Exercise #0 function
+ * @n: Te pointer of the integer
+ * Description: Takes a pointer to an int as parameter
+ * and updates the value it points to to 98
+ *
+ * Return: Always null (void)
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
