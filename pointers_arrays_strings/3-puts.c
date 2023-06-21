12ba62e0010cab049be7418dcf737ce531694af6
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-21 11:13:17 -0300

    Exercise #3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - Exercise #3 function
+ * @str: The string to print its content
+ * Description: rints a string, followed by a
+ * new line, to stdout
+ *
+ * Return: void
+*/
+
+void _puts(char *str)
+{
+	puts(str);
+}
