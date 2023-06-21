fa9a5c4ef53d8e0074023755bc8c4160d17fa307
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-21 11:10:30 -0300

    Exercise #2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - Exercise #3 function
+ * @s: The string to return the lenght of
+ * Description: returns the length of a string
+ *
+ * Return: The length of the string
+*/
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
