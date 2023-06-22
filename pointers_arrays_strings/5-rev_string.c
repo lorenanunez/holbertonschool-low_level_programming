553c34d7817f916e027ff3dfe81785a4e928fe60
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-22 18:16:16 -0300

    Exericse #5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - Exercise #5 function
+ * @s: The string to reverse
+ * Description: Reverses a string
+ *
+ * Return: The reversed string
+*/
+
+void rev_string(char *s)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < strlen(s) / 2; i++)
+	{
+		c = s[strlen(s) - i - 1];
+		s[strlen(s) - i - 1] = s[i];
+		s[i] = c;
+	}
+}
