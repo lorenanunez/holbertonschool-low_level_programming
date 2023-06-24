fc774f8b8330c82043b4f073bb0f64f1af7a7efe
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-24 11:20:32 -0300

    Exercise #7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - Exercise #7 function
+ * @str: The string to print the half
+ * Description: Prints half of the string
+ *
+ * Return: Always void
+*/
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = strlen(str) / 2; i < (int) strlen(str); i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
