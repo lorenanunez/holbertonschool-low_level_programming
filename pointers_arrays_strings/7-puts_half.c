0f6c19a411c214167550383180dbe50bb785684e
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-24 11:22:11 -0300

    Exercise #7 fix

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,7 @@ void puts_half(char *str)
 {
 	int i;
 
-	for (i = strlen(str) / 2; i < (int) strlen(str); i++)
+	for (i = (strlen(str) - 1) / 2; i < (int) strlen(str); i++)
 	{
 		putchar(str[i]);
 	}
