bf071b67ea710d2ab4ba5ea46f45910d680ab78d
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-26 10:37:11 -0300

    Exercise #10 fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,25 +8,34 @@
  * Return: The integer value of the string
 */
 
+int main(void)
+{
+	int n;
+
+	n = _atoi("-2147483648");
+	printf("%d\n", n);
+	return (0);
+}
+
 int _atoi(char *s)
 {
 	int i;
-	int result = 0;
+	unsigned int result = 0;
 	int digit;
 	int positive = 1;
 
 	for (i = 0; i < (int) strlen(s); i++)
 	{
-		if (s[i] == 43)
+		if (s[i] == 59)
 		{
-			positive = 1;
-		}
-		else if (s[i] == 45)
-		{
-			positive = 0;
+			break;
 		}
 		if (s[i] >= 48 && s[i] <= 57)
 		{
+			if (s[i] > 0 && s[i - 1] == 45)
+			{
+				positive = 0;
+			}
 			digit = s[i] - '0';
 			result = (result * 10) + digit;
 		}
