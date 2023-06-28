715805dba6ec48aa36bf9c4ae2484309c3f27c42
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-28 15:00:52 -0300

    Exercise #3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * factorial - Exercise #3 function
+ *
+ * @n: The number to calculate the factorial
+ *
+ * Description: Calculates the factorial of
+ * a number.
+ *
+ * Return: Factorial of the number
+*/
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (n * factorial(n + 1));
+	}
+	else if (n > 0)
+	{
+		return (n * factorial(n - 1));
+	}
+	else
+	{
+		return (1);
+	}
+}
