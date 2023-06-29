1ced77beeec98adff35d0502f873175e983e5907
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-29 10:48:36 -0300

    Exercise #5

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - Exercise #5 function
+ *
+ * @n: The number to calculate the square root
+ *
+ * Description: Calculates the square root of
+ * an integer.
+ *
+ * Return: The square root of a number. If it
+ * does not have a natural square root, it
+ * returns -1.
+*/
+
+int _sqrt_recursion(int n)
+{
+	calculate(n, 1, n);
+}
+
+/**
+ * calculate - Exercise #5 function helper
+ *
+ * @n: The number to calculate the square root
+ * @min: The minimum value to guess
+ * @max: The max value to guess
+ *
+ * Description: Helps exercise #5 function to
+ * calculate the square root of a number.
+ *
+ * Return: The square root of a number. If it
+ * does not have a natural square root, it
+ * returns -1.
+*/
+
+int calculate(int n, int min, int max)
+{
+	int guess;
+	int guess_powered;
+
+	if (max < min)
+	{
+		return (-1);
+	}
+	guess = (min + max) / 2;
+	guess_powered = guess * guess;
+	if (guess_powered == n)
+	{
+		return ((int) guess);
+	}
+	else if (guess_powered < n)
+	{
+		calculate(n, guess + 1, max);
+	}
+	else
+	{
+		calculate(n, min, guess - 1);
+	}
+}
