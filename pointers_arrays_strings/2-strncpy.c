a7a149aae57accc3b5dbd3db65edb45a43d58ca1
Lorena Nuñez <lorenunez@vera.com.uy>
2023-06-30 13:58:10 -0300

    Exerxise #2 fix

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,5 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
-
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	{
-		dest[i] = src[i];
-	}
-	return (dest);
+	return (strncpy(dest, src, n));
 }
