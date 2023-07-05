a67bfe70f3f8c9dd917b9a039a468aa38d51b34f
Lorena Nuñez <67658316+lorenanunez@users.noreply.github.com>
2023-07-05 11:15:31 -0300

    Exercise #1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strdup - Exercise #1 function
+ *
+ * @str: The string to duplicate
+ *
+ * Description: Returns a pointer to a newly allocated
+ * space in memory, which contains a copy of the string
+ * given as a parameter.
+ *
+ * Return: A pointer to a newly allocated space in memory
+*/
+
+char *_strdup(char *str)
+{
+	int i;
+	char *str_duplicated;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		str_duplicated = malloc(strlen(str) * sizeof(char));
+		for (i = 0; i < (int) strlen(str); i++)
+		{
+			str_duplicated[i] = str[i];
+		}
+		return (str_duplicated);
+	}
+}
