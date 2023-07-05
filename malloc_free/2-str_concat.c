1164ef2259a1ec5b21dbe0cf405f7e0b0a109086
Lorena Nuñez <67658316+lorenanunez@users.noreply.github.com>
2023-07-05 14:09:07 -0300

    Exercise #2 fix

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,24 +16,26 @@
 char *str_concat(char *s1, char *s2)
 {
 	int i;
-	int pos;
+	int s1length = strlen(s1);
+	int s2length = strlen(s2);
+	int strpos = 0;
 	char *str = NULL;
-	int length = strlen(s1) + strlen(s2);
+	int length = s1length + s2length;
 
 	str = malloc(length * sizeof(char) + 1);
 	if (str != NULL)
 	{
-		for (i = 0; i < (int) strlen(s1); i++)
+		for (i = 0; i < s1length; i++)
 		{
-			str[i] = s1[i];
-			pos++;
+			str[strpos] = s1[i];
+			strpos++;
 		}
-		for (i = 0; i < (int) strlen(str); i++)
+		for (i = 0; i < length; i++)
 		{
-			str[pos] = s2[i];
-			pos++;
+			str[strpos] = s2[i];
+			strpos++;
 		}
-		str[pos] = '\0';
+		str[strpos] = '\0';
 	}
 	return (str);
 }
