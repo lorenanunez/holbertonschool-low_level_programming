62689a5d94137400a27c2def1d3e0dd3e6661fb0
Lorena Nuñez <lorenunez@vera.com.uy>
2023-07-10 15:21:27 -0300

    Exercise #2

---
+++ preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Exercise #2 function
+ *
+ * Description: Prints the name of the file
+ * it was compiled from, followed by a new
+ * line
+ *
+ * Return: Always success (0)
+ *
+ **/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
