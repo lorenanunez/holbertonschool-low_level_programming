2ebdfb8c670e4894bfeba8be997f1bdd14a6ac45
Lorena Nuñez <lorenunez@vera.com.uy>
2023-07-10 17:52:00 -0300

    Exercise #0 fix

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,18 @@
-struct dog {
+#ifndef HEADER
+#define HEADER
+/**
+ * struct dog - Men best friend
+ *
+ * @name: The name of the doggo
+ * @age: The age of the doggo
+ * @owner: The name of the doggo's owner
+ *
+ * Description: Struct to represent a dog
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
