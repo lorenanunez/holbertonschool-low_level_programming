2513ad247e755fa770b956e83bc10b065840a7b5
Lorena Nuñez <lorenunez@vera.com.uy>
2023-07-10 18:05:17 -0300

    Exercise #1

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - Exercise #2 function
+ *
+ * @d: The dog to initialize
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The name of the dog's owner
+ *
+ * Description: Initializes the dog
+ *
+ * Return: Always null
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
