f78ef1d50ce24e56284ad1aba4d368ee5df102f4
Lorena Nuñez <67658316+lorenanunez@users.noreply.github.com>
2023-07-13 10:15:30 -0300

    Exercise #4 fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stdlib.h>
 
 /**
  * new_dog - Exercise #4 function
@@ -13,12 +12,39 @@
  * Return: The created dog
 */
 
+int main(void)
+{
+	char *name = "Ghost";
+	char *owner = "Jon Snow";
+	dog_t *my_dog;
+
+	my_dog = new_dog(name, 4.75, owner);
+	if (my_dog == NULL)
+	{
+		printf("Failed\n");
+		return (1);
+	}
+	if (my_dog->name == name || my_dog->owner == owner)
+	{
+		printf("Duplicated strings should be stored in the allocated structure.\n");
+		return (1);
+	}
+	printf("My name is %s, I am %.2f, and my owner is %s\n",
+		my_dog->name, my_dog->age, my_dog->owner);
+	return (0);
+}
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *theDog = malloc(sizeof(dog_t));
 
-	theDog->name = name;
-	theDog->age = age;
-	theDog->owner = owner;
+	if (theDog != NULL)
+	{
+		theDog->name = malloc(sizeof(name));
+		theDog->owner = malloc(sizeof(owner));
+		strcpy(theDog->name, name);
+		theDog->age = age;
+		strcpy(theDog->owner, owner);
+	}
 	return (theDog);
 }
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef HEADER
 #define HEADER
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct dog - Men best friend
  *
