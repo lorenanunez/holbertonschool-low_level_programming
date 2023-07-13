2dc20cf6cc69af5236a2be01fdcd2bcf20c90d53
Lorena Nuñez <67658316+lorenanunez@users.noreply.github.com>
2023-07-13 10:15:59 -0300

    Exercise #4 fix

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,28 +12,6 @@
  * Return: The created dog
 */
 
-int main(void)
-{
-	char *name = "Ghost";
-	char *owner = "Jon Snow";
-	dog_t *my_dog;
-
-	my_dog = new_dog(name, 4.75, owner);
-	if (my_dog == NULL)
-	{
-		printf("Failed\n");
-		return (1);
-	}
-	if (my_dog->name == name || my_dog->owner == owner)
-	{
-		printf("Duplicated strings should be stored in the allocated structure.\n");
-		return (1);
-	}
-	printf("My name is %s, I am %.2f, and my owner is %s\n",
-		my_dog->name, my_dog->age, my_dog->owner);
-	return (0);
-}
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *theDog = malloc(sizeof(dog_t));
@@ -42,9 +20,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 		theDog->name = malloc(sizeof(name));
 		theDog->owner = malloc(sizeof(owner));
-		strcpy(theDog->name, name);
-		theDog->age = age;
-		strcpy(theDog->owner, owner);
+		if (theDog->name != NULL && theDog->owner != NULL)
+		{
+			strcpy(theDog->name, name);
+			theDog->age = age;
+			strcpy(theDog->owner, owner);
+		}
 	}
 	return (theDog);
 }
