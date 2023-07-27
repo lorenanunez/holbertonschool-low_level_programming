33b5daed67210ffaf6510c7c98e21ac05fe4fea8
Lorena Nuñez <67658316+lorenanunez@users.noreply.github.com>
2023-07-27 15:51:27 -0300

    Temporal fix

---
+++ singly_linked_lists/sfgdg.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_list - Exercise #0 function
+ *
+ * @h: The list to print
+ *
+ * Description: Prints all the elements of a list
+ * and then returns the number of nodes.
+ *
+ * Return: The number of the list nodes.
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t list_size = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		list_size++;
+		h = h->next;
+	}
+	return (list_size);
+}
