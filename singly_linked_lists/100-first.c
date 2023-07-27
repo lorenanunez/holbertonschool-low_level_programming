47177e5eb9cacda4e8f7a6fe8860506eed4e3210
Lorena Nuñez <67658316+lorenanunez@users.noreply.github.com>
2023-07-27 15:41:17 -0300

    Exercise #5 function

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * before_main - Exercise #5 function.
+ *
+ * Description: Prints a string before the main
+ * function is executed. This is done by putting
+ * attribute for the function. When the attribute
+ * is constructor, the function will be executed
+ * before main().
+ *
+ * Return: Always void.
+*/
+
+void before_main() __attribute__((constructor));
+void before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
