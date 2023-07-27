84e4657493c46076f464f53a38f2ac1775ed9fb0
Lorena Nuñez <67658316+lorenanunez@users.noreply.github.com>
2023-07-27 15:50:13 -0300

    Fix betty issues

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -12,8 +12,7 @@
  * Return: Always void.
 */
 
-void before_main() __attribute__((constructor));
-void before_main()
+void __attribute__((constructor)) before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
